04b1e255093993ccd4455997661559b50cb6ec50
Samuel Olugbemi <luvolugbemmy@gmail.com>
2020-06-10 04:45:28 +0100

    Module#parent_name deprecation warnings for rails 6
    Changed Module#parent_name to module_parent_name

--- lib/we/call/connection.rb
+++ lib/we/call/connection.rb
@@ -136,7 +136,7 @@ module We
 
       def rails_app_name
         if (defined? ::Rails) && !::Rails.application.nil?
-          ::Rails.application.class.parent_name.underscore.dasherize
+          ::Rails.application.class.module_parent_name.underscore.dasherize
         end
       end
     end
